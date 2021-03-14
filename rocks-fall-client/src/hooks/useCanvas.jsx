70079727219c3b51cb4bef088e648c01242dcd26
Solomon R Feldman <SolomonRFeldman@gmail.com>
2021-03-14 13:44:32 -0400

    adds map

--- rocks-fall-client/src/hooks/useCanvas.jsx
+++ rocks-fall-client/src/hooks/useCanvas.jsx
@@ -1,13 +1,18 @@
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
+import useImage from './useImage'
 
 export default function useCanvas() {
   const canvasRef = useRef(null)
+  const [map] = useImage('../test_map.png')
 
   useEffect(() => {
     const canvasObj = canvasRef.current
     const ctx = canvasObj.getContext('2d')
+
     ctx.fillStyle = 'gray'
     ctx.fillRect(0,0, window.innerWidth, window.innerHeight)
+
+    ctx.drawImage(map, 0, 0, map.width, map.height)
   })
 
   return(
