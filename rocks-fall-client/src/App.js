a35b816d7ca820a2fbf776b77f22d66fa86d3f37
Solomon R Feldman <SolomonRFeldman@gmail.com>
2021-03-14 12:23:19 -0400

    comments out websockets for future use

--- rocks-fall-client/src/App.js
+++ rocks-fall-client/src/App.js
@@ -4,48 +4,48 @@ import React, { useEffect, useState } from 'react'
 
 
 function App() {
-  const [client, setClient] = useState(new W3CWebSocket('ws://localhost:3001/cable'))
-  const [text, setText] = useState('')
+  // const [client, setClient] = useState(new W3CWebSocket('ws://localhost:3001/cable'))
+  // const [text, setText] = useState('')
 
-  useEffect(() => {
-    client.onopen = () => {
-      console.log('WebSocket Client Connected');
-      client.send(JSON.stringify({
-        command: 'subscribe',
-        identifier: JSON.stringify({ channel: 'BoardChannel' })
-      }))
-    };
-    client.onmessage = (message) => {
-      const data = JSON.parse(message.data)
-      console.log(data.type)
-      if (data.type !== 'ping' && data.type !== 'welcome' && data.type !== 'confirm_subscription') {
-        console.log(data)
-        setText(data.message.text)
-      }
-    };
-    client.onclose = (message) => {
-      setClient(new W3CWebSocket('ws://localhost:3001/cable'))
-      console.log(message)
-    };
-  }, [client])
+  // useEffect(() => {
+  //   client.onopen = () => {
+  //     console.log('WebSocket Client Connected');
+  //     client.send(JSON.stringify({
+  //       command: 'subscribe',
+  //       identifier: JSON.stringify({ channel: 'BoardChannel' })
+  //     }))
+  //   };
+  //   client.onmessage = (message) => {
+  //     const data = JSON.parse(message.data)
+  //     console.log(data.type)
+  //     if (data.type !== 'ping' && data.type !== 'welcome' && data.type !== 'confirm_subscription') {
+  //       console.log(data)
+  //       setText(data.message.text)
+  //     }
+  //   };
+  //   client.onclose = (message) => {
+  //     setClient(new W3CWebSocket('ws://localhost:3001/cable'))
+  //     console.log(message)
+  //   };
+  // }, [client])
 
 
-  const handleChange = event => {
-    setText(event.target.value)
-    client.send(JSON.stringify({ 
-      command: 'message',
-      identifier: JSON.stringify({ channel: 'BoardChannel' }),
-      data: JSON.stringify({ text: event.target.value })
-    }))
-  }
+  // const handleChange = event => {
+  //   setText(event.target.value)
+    // client.send(JSON.stringify({ 
+    //   command: 'message',
+    //   identifier: JSON.stringify({ channel: 'BoardChannel' }),
+    //   data: JSON.stringify({ text: event.target.value })
+    // }))
+  // }
 
-  useEffect(() => {
-    console.log(text)
-  }, [text])
+  // useEffect(() => {
+  //   console.log(text)
+  // }, [text])
 
   return (
     <div className="App">
-      <input onChange={handleChange} value={text}></input>
+      {/* <input onChange={handleChange} value={text}></input> */}
     </div>
   );
 }
