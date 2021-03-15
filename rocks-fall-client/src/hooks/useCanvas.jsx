a324eaa7973d0b5b1aaa4d476050003aa1e54363
Solomon R Feldman <SolomonRFeldman@gmail.com>
2021-03-14 20:49:35 -0400

    adds test token

--- rocks-fall-client/src/hooks/useCanvas.jsx
+++ rocks-fall-client/src/hooks/useCanvas.jsx
@@ -4,6 +4,7 @@ import useImage from './useImage'
 export default function useCanvas() {
   const canvasRef = useRef(null)
   const [map] = useImage('../test_map.png')
+  const [token] = useImage('../test_token.png')
 
   useEffect(() => {
     const canvasObj = canvasRef.current
@@ -13,6 +14,7 @@ export default function useCanvas() {
     ctx.fillRect(0,0, window.innerWidth, window.innerHeight)
 
     ctx.drawImage(map, 0, 0, map.width, map.height)
+    ctx.drawImage(token, 100, 100, 50, 50)
   })
 
   return(
