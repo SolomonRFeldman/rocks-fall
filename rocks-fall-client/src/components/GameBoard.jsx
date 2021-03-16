72e6013c63621b4c4a2a722318aac17264e055ed
Solomon R Feldman <SolomonRFeldman@gmail.com>
2021-03-15 21:49:27 -0400

    adds dragging canvas

--- rocks-fall-client/src/components/GameBoard.jsx
+++ rocks-fall-client/src/components/GameBoard.jsx
@@ -6,8 +6,12 @@ export default function GameBoard() {
   const [ctx, setCtx] = useState(null)
   const [width, setWidth] = useState(window.innerWidth)
   const [height, setHeight] = useState(window.innerHeight)
+  const [translationX, setTranslationX] = useState(0)
+  const [translationY, setTranslationY] = useState(0)
   const [zoomIncrement, setZoomIncrement] = useState(0)
   const [scale, setScale] = useState(1)
+
+  const [mouseDown, setMouseDown] = useState(false)
 
   const [map] = useImage('../test_map.png')
   const [token] = useImage('../test_token.png')
@@ -21,18 +25,19 @@ export default function GameBoard() {
       setHeight(window.innerHeight)
     }
     window.addEventListener('resize', handleResize)
+    window.addEventListener('mouseup', () => setMouseDown(false))
     return () => {
       window.removeEventListener('resize', handleResize)
+      window.removeEventListener('mouseup', () => setMouseDown(false))
     }
   }, [])
 
   const draw = () => {
-    console.log('drawing')
-    ctx.setTransform(scale, 0, 0, scale, 0, 0)
-    const currentScale = ctx.getTransform().a
-
+    // console.log('drawing')
+    ctx.setTransform(scale, 0, 0, scale, translationX, translationY)
     ctx.fillStyle = 'gray'
-    ctx.fillRect(0,0, (1 / currentScale) * width, (1 / currentScale) * height)
+
+    ctx.fillRect(-translationX / scale, -translationY / scale, width / scale, height / scale)
 
     ctx.drawImage(map, 0, 0, map.width, map.height)
     ctx.drawImage(token, 0, 0, 50, 50)
@@ -52,6 +57,13 @@ export default function GameBoard() {
   const handleMouseEnter = () => canvasRef.current.addEventListener('wheel', event => event.preventDefault(), { passive: false })
   const handleMouseLeave = () => canvasRef.current.removeEventListener('wheel', event => event.preventDefault(), { passive: false })
 
+  const handleMouseDown = () => setMouseDown(true)
+  const handleMouseMove = event => {
+    if (mouseDown) {
+      setTranslationX(translationX + event.movementX)
+      setTranslationY(translationY + event.movementY)
+    }
+  }
   return(
     <canvas
       className="App-canvas"
@@ -61,6 +73,8 @@ export default function GameBoard() {
       style={{border: "1px solid #000000"}}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
       onWheel={handleWheel}
     />
   )
